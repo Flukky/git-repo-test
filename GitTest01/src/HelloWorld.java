2aea8f6fefa69496a4c0ab56373de6f67143f80a
JLE <janis.leimanis24@gmail.com>
2018-07-25 08:20:11 +0300

    second line added

--- GitTest01/src/HelloWorld.java
+++ GitTest01/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Git project 01");
+		System.out.println("Hell yeah, it's fucking easy!");
 	}
 
 }
